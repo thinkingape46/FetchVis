06d4ebd693015a70c8413ec20ceb93122e1e9726
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-12-04 22:07:39 +0530

    continue working on react webpack setup

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
-console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
 
-document.querySelector("h1").innerHTML = "Hello World!!";
+function App() {
+  return <h1>Hello from React</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
 
 if (module.hot) {
   module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,50 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
-module.exports = {
+const config = {
   entry: "./app/app.js",
   output: {
-    filename: "myBundle.js",
+    filename: "myBundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  mode: "development",
+  plugins: [new HtmlWebPackPlugin({ template: "./app/index.html" })],
   devServer: {
     port: 8080,
     contentBase: path.resolve(__dirname, "dist"),
     hot: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
+
+if (currentTask == "build") {
+  config.mode = "production";
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
+  config.plugins.push(
+    new MiniCssExtractPlugin({ filename: "main.[hash].css" }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  );
+}
+module.exports = config;
