8f721eb4ef25b6ded69c934d671d557de6587c79
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-12-08 22:06:16 +0530

    working to connect with strava API

--- app/components/DateRange.js
+++ app/components/DateRange.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from "react";
+import currentDate from "../scripts/currentDate";
 
 function DateRange() {
   return (
     <>
       <div className="date-range box">
         <input
-          className="date-range__date box box--inset-tiny text text--medium text--tiny"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+          className="date-range__date  box box--inset-tiny text text--normal text--tiny"
           type="date"
           name="start-date"
+          defaultValue={currentDate()}
           id=""
         />
         <input
-          className="date-range__date box box--inset-tiny text text--medium text--tiny"
+          className="date-range__date box box--inset-tiny text text--normal text--tiny"
           type="date"
           name="end-date"
+          defaultValue={currentDate()}
           id=""
         />
       </div>
