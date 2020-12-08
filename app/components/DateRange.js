18052fedccc0aba11eb6eca5e9ac30a3188c4097
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-12-08 23:57:37 +0530

    working with strava

--- app/components/DateRange.js
+++ app/components/DateRange.js
@@ -1,14 +1,26 @@
 import React, { useEffect } from "react";
+import Axios from "axios";
 import currentDate from "../scripts/currentDate";
+
+async function connectStrava(e) {
+  try {
+    const response = await Axios.get("https://www.strava.com/api/v3/athlete", {
+      headers: {
+        authorization: "Bearer ACCESSTOKEN",
+      },
+    });
+    console.log(`${response.data.firstname} ${response.data.lastname}`);
+  } catch (e) {
+    console.log("error");
+  }
+}
 
 function DateRange() {
   return (
     <>
       <div className="date-range box">
         <input
-          onChange={(e) => {
-            console.log(e.target.value);
-          }}
+          onChange={(e) => connectStrava(e)}
           className="date-range__date  box box--inset-tiny text text--normal text--tiny"
           type="date"
           name="start-date"
