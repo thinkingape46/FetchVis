b7cf79293b744c2a9cbb976789db89b0c7bce505
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-05-25 23:58:50 +0530

    Updated App.js

--- app/pages/App.js
+++ app/pages/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENT IMPORTS
 import Page from "../components/Page";
@@ -12,13 +12,23 @@ import Footer from "../components/Footer";
 
 // REDUX ACTIONS IMPORT
 import { USERDATA } from "../store/userProfileReducer";
+import { STRAVATOKEN } from "../store/stravaTokensReducer";
 
-function Login() {
+function Login(props) {
   // REDUX HOOKS START
   const dispatch = useDispatch();
+  const accessToken = useSelector(
+    (store) => store.stravaTokenReducer.accessToken
+  );
+  const authorizationCode = useSelector(
+    (store) => store.authReducer.authorizationCode
+  );
   // REDUX HOOKS END
   // MAKING A REQUEST FOR ACCESS TOKEN START
   useEffect(() => {
+    if (authorizationCode === "") {
+      props.history.push("/");
+    }
     const postUrl = `https://www.strava.com/oauth/token?client_id=${
       process.env.CLIENTID
     }&client_secret=${process.env.CLIENTSECRET}&code=${localStorage.getItem(
@@ -45,6 +55,16 @@ function Login() {
             firstName: response.data.athlete.firstname,
             lastName: response.data.athlete.lastname,
             profilePicture: response.data.athlete.profile,
+          },
+        });
+        dispatch({
+          type: STRAVATOKEN,
+          tokenData: {
+            accessToken: response.data.access_token,
+            expiresAt: response.data.expires_at,
+            expiresIn: response.data.expires_in,
+            refreshToken: response.data.refresh_token,
+            tokenType: response.data.token_type,
           },
         });
       })
