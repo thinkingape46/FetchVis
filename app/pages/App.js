c9e900d9ce21a0a84c3b39d963b1f9e1b7edf31b
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-05-26 00:49:10 +0530

    Application will send a fetch request only when access_token is not present in sessionStorage

--- app/pages/App.js
+++ app/pages/App.js
@@ -17,58 +17,61 @@ import { STRAVATOKEN } from "../store/stravaTokensReducer";
 function Login(props) {
   // REDUX HOOKS START
   const dispatch = useDispatch();
-  const accessToken = useSelector(
-    (store) => store.stravaTokenReducer.accessToken
-  );
-  const authorizationCode = useSelector(
-    (store) => store.authReducer.authorizationCode
-  );
+  const accessToken = sessionStorage.getItem("strava_access_token");
+  const authorizationCode = sessionStorage.getItem("authorizationCode");
   // REDUX HOOKS END
   // MAKING A REQUEST FOR ACCESS TOKEN START
   useEffect(() => {
     if (authorizationCode === "") {
       props.history.push("/");
     }
-    const postUrl = `https://www.strava.com/oauth/token?client_id=${
-      process.env.CLIENTID
-    }&client_secret=${process.env.CLIENTSECRET}&code=${localStorage.getItem(
-      "authorizationCode"
-    )}&grant_type=authorization_code`;
+    if (accessToken === "") {
+      const postUrl = `https://www.strava.com/oauth/token?client_id=${process.env.CLIENTID}&client_secret=${process.env.CLIENTSECRET}&code=${authorizationCode}&grant_type=authorization_code`;
 
-    Axios.post(postUrl)
-      .then((response) => {
-        console.log(response.data);
-        localStorage.setItem("strava_access_token", response.data.access_token);
-        localStorage.setItem(
-          "strava_refresh_token",
-          response.data.refresh_token
-        );
-        localStorage.setItem(
-          "currentAthlete",
-          JSON.stringify(response.data.athlete)
-        ),
-          localStorage.setItem("strava_expires-at", response.data.expires_at),
-          localStorage.setItem("strava_expires-in", response.data.expires_in);
-        dispatch({
-          type: USERDATA,
-          userData: {
-            firstName: response.data.athlete.firstname,
-            lastName: response.data.athlete.lastname,
-            profilePicture: response.data.athlete.profile,
-          },
-        });
-        dispatch({
-          type: STRAVATOKEN,
-          tokenData: {
-            accessToken: response.data.access_token,
-            expiresAt: response.data.expires_at,
-            expiresIn: response.data.expires_in,
-            refreshToken: response.data.refresh_token,
-            tokenType: response.data.token_type,
-          },
-        });
-      })
-      .catch((error) => console.log(error));
+      Axios.post(postUrl)
+        .then((response) => {
+          console.log(response.data);
+          sessionStorage.setItem(
+            "strava_access_token",
+            response.data.access_token
+          );
+          sessionStorage.setItem(
+            "strava_refresh_token",
+            response.data.refresh_token
+          );
+          sessionStorage.setItem(
+            "currentAthlete",
+            JSON.stringify(response.data.athlete)
+          ),
+            sessionStorage.setItem(
+              "strava_expires-at",
+              response.data.expires_at
+            ),
+            sessionStorage.setItem(
+              "strava_expires-in",
+              response.data.expires_in
+            );
+          dispatch({
+            type: USERDATA,
+            userData: {
+              firstName: response.data.athlete.firstname,
+              lastName: response.data.athlete.lastname,
+              profilePicture: response.data.athlete.profile,
+            },
+          });
+          dispatch({
+            type: STRAVATOKEN,
+            tokenData: {
+              accessToken: response.data.access_token,
+              expiresAt: response.data.expires_at,
+              expiresIn: response.data.expires_in,
+              refreshToken: response.data.refresh_token,
+              tokenType: response.data.token_type,
+            },
+          });
+        })
+        .catch((error) => console.log(error));
+    }
   }, []);
   // MAKING A REQUEST FOR ACCESS TOKEN END
   return (
